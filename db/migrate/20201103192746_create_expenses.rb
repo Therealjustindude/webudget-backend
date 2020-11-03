03d3d56276d9963242894b10b0f85cd1359a203b
therealjustindude <therealjustindude@gmail.com>
2020-11-03 13:32:54 -0600

    resource for User and Expense model created

---
+++ db/migrate/20201103192746_create_expenses.rb
@@ -0,0 +1,16 @@
+class CreateExpenses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :expenses do |t|
+      t.integer :user_id
+      t.string :description
+      t.float :amount
+      t.date :date_due
+      t.string :bank_account
+      t.boolean :is_automatic
+      t.boolean :is_paid
+      t.boolean :is_money_in_account
+
+      t.timestamps
+    end
+  end
+end
