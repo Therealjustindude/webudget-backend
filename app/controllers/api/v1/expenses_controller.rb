916d2cf019208b2739358a566a731057a927e863
therealjustindude <therealjustindude@gmail.com>
2020-11-06 14:40:21 -0600

    added contoller actions -still need work-

--- app/controllers/api/v1/expenses_controller.rb
+++ app/controllers/api/v1/expenses_controller.rb
@@ -1,3 +1,44 @@
 class Api::V1::ExpensesController < ApplicationController
+	before_action :set_expense, only: [:show, :update, :destroy]
 
+	def index	
+		@expenses = Expense.all
+		render json: accounts
+	end
+
+	def show
+		render json: @expense 
+	end
+
+	def create
+		@expense = Expense.new(exspense_params)
+		#add logic to make sure user is logged in and able to create this expense
+		if @account.save 
+			render json: @expense
+		else
+			render json: @account.errors, status: :unprocessable_entity
+		end
+		
+	end
+
+	def update
+		if @expense.update(:paid => true)
+			render json: @expense
+		  else
+			render json: @expense.errors, status: :unprocessable_entity
+		  end
+	end
+
+	def destroy
+		@expense.destroy
+	end
+
+
+	private
+	def set_expense
+		@expense = Expense.find(params[:id])
+	end
+	def exspense_params
+		params.require(:expenses).permit(:id, :user_id, :description, :amount, :date_due, :bank_account, :is_automatic, :is_paid, :is_money_in_account)
+	end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,41 @@
 class Api::V1::UsersController < ApplicationController
-	
-	
-	  private
-	  def user_params
+	before_action :set_user, only: [:show, :update, :destroy]
+	def index
+		@user = User.all
+		render json: @user
+	end
+
+	def show
+		render json: @user 
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save 
+			render json: @user
+		else
+			render json: @user.errors, status: :unprocessable_entity
+		end
+		
+	end
+
+	def update
+		if @user.update(:paid => true)
+			render json: @user
+		  else
+			render json: @user.errors, status: :unprocessable_entity
+		  end
+	end
+
+	def destroy
+		@user.destroy
+	end
+
+	private
+	def set_user
+		@user = User.find(params[:id])
+	end
+	def user_params
 		params.require(:user).permit(:username, :password, :email)
-	  end
+	end
 end
