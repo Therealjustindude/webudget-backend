08a01e0e7f69406f33c6aaef86fe2dc15b37ad8e
therealjustindude <therealjustindude@gmail.com>
2020-11-19 09:02:30 -0600

    index and show removed, create action adds a auth token and renders json with user info and token

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,18 +1,16 @@
 class Api::V1::UsersController < ApplicationController
 	before_action :set_user, only: [:show, :update, :destroy]
-	def index
-		@user = User.all
-		render json: @user
-	end
 
-	def show
-		render json: @user 
-	end
+	# def show
+	# 	render json: @user 
+	# end
 
 	def create
 		@user = User.new(user_params)
 		if @user.save 
-			render json: @user
+			binding.pry
+			token = JsonWebToken.encode(user_id: @user.id)
+			render json: { user: @user, auth_token: token }
 		else
 			render json: @user.errors, status: :unprocessable_entity
 		end
@@ -35,7 +33,8 @@ class Api::V1::UsersController < ApplicationController
 	def set_user
 		@user = User.find(params[:id])
 	end
+	
 	def user_params
-		params.require(:user).permit(:username, :password, :email)
+		params.require(:user).permit(:user_name, :password, :email)
 	end
 end
