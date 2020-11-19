6b8cf7ca7c2cbce102ec1bcd246456a152406298
therealjustindude <therealjustindude@gmail.com>
2020-11-19 08:59:20 -0600

    user auth and json returned with user and auth token

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -2,12 +2,19 @@ class Api::V1::AuthenticationController < ApplicationController
 	skip_before_action :authenticate_request
    
 	def authenticate
-	  command = AuthenticateUser.call(params[:email], params[:password])
-   
+	  command = AuthenticateUser.call(auth_params[:email], auth_params[:password])
 	  if command.success?
-		render json: { auth_token: command.result }
+		user = User.find_by_email(command.email)
+		render json: { user: user, auth_token: command.result }
 	  else
 		render json: { error: command.errors }, status: :unauthorized
 	  end
 	end
-   end
+
+	private
+	def auth_params
+		params.require(:authentication).permit(:email, :password)
+	end
+end
+
+   
