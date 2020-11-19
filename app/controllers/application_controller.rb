c845a6578eebdc670871b4cede0f81143b2955e9
therealjustindude <therealjustindude@gmail.com>
2020-11-19 09:00:47 -0600

    method to skip auth is its a post request added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-	before_action :authenticate_request
+	before_action :authenticate_request, unless: :this_is_true?
 	attr_reader :current_user
   
 	private
@@ -7,5 +7,9 @@ class ApplicationController < ActionController::API
 	def authenticate_request
 	  @current_user = AuthorizeApiRequest.call(request.headers).result
 	  render json: { error: 'Not Authorized' }, status: 401 unless @current_user
+	end
+
+	def this_is_true?
+		request.headers['method'] === "USER_POST"
 	end
 end
