61558a70bd7caefd52595a9404725534dcf48fe6
therealjustindude <therealjustindude@gmail.com>
2020-11-23 13:24:39 -0600

    create action finds user build an expense and uses serialize_has to only render expense json without association

--- app/controllers/api/v1/expenses_controller.rb
+++ app/controllers/api/v1/expenses_controller.rb
@@ -11,10 +11,11 @@ class Api::V1::ExpensesController < ApplicationController
 	end
 
 	def create
-		@expense = Expense.new(expense_params)
+		user = User.find_by(id: expense_params['user_id'])
+		expense = user.expenses.build(expense_params)
 		#add logic to make sure user is logged in and able to create this expense
-		if @expense.save 
-			render json: @expense
+		if expense.save 
+			render json: expense.serializable_hash(only: [:id, :user_id, :description, :amount, :date_due, :bank_account, :is_automatic, :is_paid, :is_money_in_account])
 		else
 			render json: @expense.errors, status: :unprocessable_entity
 		end
@@ -39,7 +40,7 @@ class Api::V1::ExpensesController < ApplicationController
 		@expense = Expense.find(params[:id])
 	end
 	
-	def exspense_params
-		params.require(:expenses).permit(:id, :user_id, :description, :amount, :date_due, :bank_account, :is_automatic, :is_paid, :is_money_in_account)
+	def expense_params
+		params.require(:expense).permit(:id, :user_id, :description, :amount, :date_due, :bank_account, :is_automatic, :is_paid, :is_money_in_account)
 	end
 end
