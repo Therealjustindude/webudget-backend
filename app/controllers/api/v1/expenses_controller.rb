2a566c065c358a942613cd337d158970fc0c1afa
therealjustindude <therealjustindude@gmail.com>
2020-11-25 12:01:41 -0600

    update action find the expense and updates correctly

--- app/controllers/api/v1/expenses_controller.rb
+++ app/controllers/api/v1/expenses_controller.rb
@@ -1,30 +1,20 @@
 class Api::V1::ExpensesController < ApplicationController
-	before_action :set_expense, only: [:show, :update, :destroy]
-
-	def index	
-		@expenses = Expense.all
-		render json: @expenses
-	end
-
-	def show
-		render json: @expense 
-	end
+	before_action :set_expense, only: [:update, :destroy]
 
 	def create
 		user = User.find_by(id: expense_params['user_id'])
 		expense = user.expenses.build(expense_params)
-		#add logic to make sure user is logged in and able to create this expense
 		if expense.save 
 			render json: expense.serializable_hash(only: [:id, :user_id, :description, :amount, :date_due, :bank_account, :is_automatic, :is_paid, :is_money_in_account])
 		else
-			render json: @expense.errors, status: :unprocessable_entity
+			render json: expense.errors, status: :unprocessable_entity
 		end
 		
 	end
 
 	def update
-		if @expense.update(:paid => true)
-			render json: @expense
+		if @expense.update_attributes(expense_params)
+			render json: @expense.serializable_hash(only: [:id, :user_id, :description, :amount, :date_due, :bank_account, :is_automatic, :is_paid, :is_money_in_account])
 		  else
 			render json: @expense.errors, status: :unprocessable_entity
 		  end
@@ -37,7 +27,7 @@ class Api::V1::ExpensesController < ApplicationController
 
 	private
 	def set_expense
-		@expense = Expense.find(params[:id])
+		@expense = Expense.find(expense_params[:id])
 	end
 	
 	def expense_params
