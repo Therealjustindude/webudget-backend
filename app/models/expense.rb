447e642d587b61dd0f311299d1ce9bc379a09e97
therealjustindude <therealjustindude@gmail.com>
2020-11-30 21:14:14 -0600

    instance method to check if expense paid

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,4 +1,13 @@
 class Expense < ApplicationRecord
 	belongs_to :user
 	belongs_to :debt, optional: true
+
+	def is_exp_paid?
+		if self.amount === 0
+			self.is_paid = true
+		else
+			self.is_paid = false
+		end
+		self
+	end
 end
