7166812006f96089ce105c78234e8195608f2a6c
therealjustindude <therealjustindude@gmail.com>
2020-11-30 21:13:55 -0600

    instance method to check if debt paid and another to update debt total

--- app/models/debt.rb
+++ app/models/debt.rb
@@ -1,4 +1,29 @@
 class Debt < ApplicationRecord
 	belongs_to :user
 	has_many :expenses
+
+	def is_debt_paid?
+		if self.total === 0
+			self.is_paid = true
+		else
+			self.is_paid = false
+		end
+		self
+	end
+
+	def update_total(exp)
+			if exp.is_paid
+				binding.pry
+				exp_total = 0
+				exp_total += exp.amount
+				debt_total = self.total - exp_total
+				self.total = debt_total
+			else
+				exp_total = 0
+				exp_total += exp.amount
+				debt_total = self.total + exp_total
+				self.total = debt_total
+			end
+		self
+	end
 end
