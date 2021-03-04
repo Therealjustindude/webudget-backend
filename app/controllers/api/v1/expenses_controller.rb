45548b205d2b989800bb179fda00eb3107647c2d
therealjustindude <therealjustindude@gmail.com>
2021-03-04 07:50:05 -0600

    check if debt selected includes the expense

--- app/controllers/api/v1/expenses_controller.rb
+++ app/controllers/api/v1/expenses_controller.rb
@@ -9,7 +9,6 @@ class Api::V1::ExpensesController < ApplicationController
 	def create
 		user = User.find_by(id: expense_params['user_id'])
 		debt = Debt.find_by(id: expense_params['debt_id'])
-		# binding.pry
 		expense = user.expenses.build(expense_params)
 		if expense.save 
 			if debt
@@ -27,17 +26,24 @@ class Api::V1::ExpensesController < ApplicationController
 		elsif expense_params['debt_id']
 			debt_sel = Debt.find_by(id: expense_params['debt_id'])
 		end
+		
 		if @expense.update(expense_params)
 			if debt_exp_paid
-				debt_exp_paid.expenses << @expense
-				debt_exp_paid.update_total(@expense)
+				if(debt_exp_paid.expenses.include?(@expense))
+					debt_exp_paid.update_total(@expense)
+				else
+					debt_exp_paid.expenses << @expense
+					debt_exp_paid.update_total(@expense)
+				end
 				debt_exp_paid.is_debt_paid?
 				debt_exp_paid.save
 			end
-			if debt_sel
+			
+			if debt_sel && !debt_sel.expenses.include?(@expense)
 				debt_sel.expenses << @expense
 				debt_sel.save
 			end
+			
 			user = User.find_by(id: expense_params['user_id'])
 			render json: user
 		  else
